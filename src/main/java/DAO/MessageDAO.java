e31ca09e030ec3f38d63a557fe4f304d116fd97f
Gibin George <gibinmgeorge2001@gmail.com>
2024-11-08 07:24:06 +0000

    fixed error in message dao

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -52,24 +52,25 @@ public class MessageDAO {
     public Message insertMessage(Message message) {
         Connection connection = ConnectionUtil.getConnection();
         try {
-            String sql = "INSERT INTO message (account_id, content) VALUES (?, ?)";
+            String sql = "INSERT INTO message (posted_by, message_text) VALUES (?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-
-            // Set parameters
-            preparedStatement.setInt(1, message.getAccount_id());
-            preparedStatement.setString(2, message.getContent());
-
+    
+            // Set parameters using the appropriate getter methods
+            preparedStatement.setInt(1, message.getPosted_by());
+            preparedStatement.setString(2, message.getMessage_text());
+    
             preparedStatement.executeUpdate();
             ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
             if (pkeyResultSet.next()) {
                 int generated_message_id = pkeyResultSet.getInt(1);
-                return new Message(generated_message_id, message.getAccountId(), message.getContent());
+                return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return null;
     }
+    
 
     /**
      * Retrieves a message by its ID.
